e1ee027bdfd972565e36906552692fc2e50da33c
Tejas ///M <tejasm9380@gmail.com>
2025-07-15 10:59:05 +0000

    EVERY THING FIXED

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import Navbar from './pages/Navbar'
+import React from 'react'
+import { NavLink } from 'react-router'
 import Home from './pages/Home'
+import Navbar from './pages/Navbar'
 import About from './pages/About'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
-import { ToastContainer } from './pages/ui/Toast'
 import Footer from './pages/Footer'
+import { ToastContainer } from './pages/ui/Toast'
 
 const App = () => {
   return (
-    
     <>
     <Navbar/>
-    <Home/>
-    <About/>
-    <Projects/>
-    <Contact/>
-    <ToastContainer/>
-    <Footer/>
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+      <ToastContainer/>
+      <Footer />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";  //npm install @tanstack/react-query , yarn add @tanstack/react-query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./index.css";
 import App from "./App";
